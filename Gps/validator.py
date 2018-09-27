9fea619360e018f13490b667e2ea2180a71f3d2d
DeliDoktoru <yunusemrahdursun@gmail.com>
2018-09-27 16:32:08 +0300

    .

---
+++ Gps/validator.py
@@ -0,0 +1,48 @@
+from django.core.validators import validate_email
+from .models import Employee,Employer
+
+def FormLogin(a):
+    m=""
+    if a.get("email") and  a.get("pass") :
+        m+=checkEmptyData(a)
+        m+=validateEmail(a.get("email"))
+    else:
+        return "Eksik Bilgi! "
+    return m
+
+def FormRegister(a):
+    m=""
+    if a.get("email") and a.get("name") and a.get("pass") and a.get("pass1"):
+        m+=checkEmptyData(a)
+        m+=validateEmail(a.get("email"))
+        m+=checkUniqEmail(a.get("email"))
+        m+=equalPassCheck(a.get("pass"),a.get("pass1"))
+    else:
+        return "Eksik Bilgi! "
+    return m    
+
+def checkEmptyData(a):
+    for val in list(a):
+        if not val[1]:
+            return "Alanların Doldurulması Gerekmektedir. "
+    return ""
+
+def validateEmail(a):  
+    try:
+        validate_email(a)
+    except:
+        return "Geçerli Mail Adresi Giriniz! "
+    return ""
+    
+   
+def equalPassCheck(a,b):    
+    if a!=b:
+        return "Şifreler Eşleşmiyor! "
+    return ""    
+
+def checkUniqEmail(a):
+    if Employer.objects.filter(mail=a):
+        return "Bu Mail Adresi Zaten Kullanılıyor! "
+    return ""
+
+    
--- Gps/views.py
+++ Gps/views.py
@@ -3,27 +3,33 @@ from django.http import HttpResponse
 from .serializers import EmployeeSerilazer,EmployerSerilazer
 from .models import Employee,Employer
 from rest_framework import viewsets
-from django.core.validators import validate_email
-
+from .validator import FormRegister,FormLogin
+from django.shortcuts import redirect
 # Create your views here.
 def home(request):
-    return render(request, 'Pages/home.html')
+    return render(request, 'Pages/home.html',{'a':request.session['mail']})
 def login(request):  
     if request.method == 'POST':
-        message="Kayıt Oluşturuldu!"
-        if request.POST.get('type')=='k':
-            try:
-                validate_email( request.POST.get('email') )
-            except:
-                message="Geçerli Mail Adresi Giriniz!"
-            try:
-                if(request.POST.get('pass')!=request.POST.get('pass1')):
-                    raise
-            except:
-                message="Şifreler Eşleşmiyor!"                   
-            e=Employer(name=request.POST.get('name'),mail=request.POST.get('email'),password=request.POST.get('pass'),token=request.POST.get('csrfmiddlewaretoken'))
-            e.save()
-            return HttpResponse(message)                  
+        
+        if request.POST.get('type')=='kayıt':
+            message=FormRegister(request.POST)
+            if message=="":
+                e=Employer(name=request.POST.get('name'),mail=request.POST.get('email'),password=request.POST.get('pass'),token=request.POST.get('csrfmiddlewaretoken'))
+                e.save()
+                message="Kayıt Oluşturuldu! "  
+            return HttpResponse(message)     
+        elif request.POST.get('type')=='giris':
+            message=FormLogin(request.POST)
+            if message=="":
+                tmp=Employer.objects.filter(mail=request.POST.get("email"),password=request.POST.get("pass") )
+                if not tmp:
+                    return HttpResponse("Email Yada Şifre Yanlış! ")            
+                else:
+                    request.session['id'] = tmp[0].id
+                    request.session['mail'] = tmp[0].mail
+                    request.session['token'] = tmp[0].token
+                    request.session['name'] = tmp[0].name
+                    return HttpResponse("Giriş Yapılıyor! ") 
     elif request.method == 'GET':    
         return render(request, 'Pages/login.html')
  
